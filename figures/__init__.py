41313dc5266eac13b44103532660ae623640b4d6
Yi-Kuang Chen <twolights@gmail.com>
2023-11-12 23:41:14 +0800

    Added figure showing/generating command

--- figures/__init__.py
+++ figures/__init__.py
@@ -1,4 +1,20 @@
+from typing import Callable
+
 from ._common import BaseFigure
 
 from .phase_response import PhaseResponseFigure
 from .group_delay import GroupDelayFigure
+
+ALL = 'all'
+AVAILABLE_FIGURES = 'a', 'b'
+FIGURE_CLASS_MAPPING = {
+    'a': PhaseResponseFigure,
+    'b': GroupDelayFigure,
+}
+
+
+def get_figure(identifier: str, H_of_omega: Callable) -> BaseFigure:
+    if identifier not in FIGURE_CLASS_MAPPING:
+        raise RuntimeError(f'Cannot find figure ({identifier})')
+    klass = FIGURE_CLASS_MAPPING[identifier]
+    return klass(H_of_omega)

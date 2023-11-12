4e9f01d8ee95af72dc30f9c3a7ded140c8e45999
Yi-Kuang Chen <twolights@gmail.com>
2023-11-12 22:46:12 +0800

    Refactored PhaseResponseFigure to eliminate duplicated code

--- figures/phase_response.py
+++ figures/phase_response.py
@@ -1,4 +1,5 @@
-from typing import Tuple
+import functools
+from typing import Tuple, Callable
 
 import numpy as np
 
@@ -8,16 +9,22 @@ from ._functions import ARG, unwrap_ARG
 
 
 class PhaseResponseFigure(BaseFigure):
+    principal_phase_of_H: Callable
+
+    def __init__(self, H_of_omega: Callable) -> None:
+        def principal_phase_of_H(H: Callable, omega: float):
+            return ARG(H(omega))
+
+        super().__init__(H_of_omega)
+        self.principal_phase_of_H = functools.partial(principal_phase_of_H, H_of_omega)
+
     def _label_upper(self) -> Tuple[str, str]:
         return ('(a) Principal Value of Phase Response',
                 r'$\mathregular{ARG[H(e^{j\omega})]}$')
 
     def _plot_upper(self, x: np.ndarray):
-        def principal_phase_of_H(omega: float):
-            return ARG(self.H_of_omega(omega))
-
         utils.do_plot(self.upper,
-                      x, np.apply_along_axis(principal_phase_of_H, 0, x),
+                      x, np.apply_along_axis(self.principal_phase_of_H, 0, x),
                       y_limit=(-4, 4))
 
     def _label_lower(self) -> Tuple[str, str]:
@@ -25,9 +32,5 @@ class PhaseResponseFigure(BaseFigure):
                 r'$\mathregular{arg[H(e^{j\omega})]}$')
 
     def _plot_lower(self, x: np.ndarray):
-        def principal_phase_of_H(omega: float):
-            return ARG(self.H_of_omega(omega))
-
-        y = unwrap_ARG(np.apply_along_axis(principal_phase_of_H, 0, x))
-
+        y = unwrap_ARG(np.apply_along_axis(self.principal_phase_of_H, 0, x))
         utils.do_plot(self.lower, x, y)
